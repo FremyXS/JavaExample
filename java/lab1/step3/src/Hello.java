8c92c7abfaa65b6789c2c75db9a02d166026b5da
FremyXS <antonovch13@mail.ru>
2022-02-11 17:45:07 +0500

    1

---
+++ java/lab1/step3/src/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String args[]){
+        System.out.println("Hello!");
+        Dog myDog = new Dog();
+        System.out.println("Dog: " + myDog.voice());
+        System.out.println("Finished step 3");
+    }
+}
